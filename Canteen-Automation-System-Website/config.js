3ad8db805180c68f28fc1e9bd909808a4ae0ea61
gowthamlakshman94 <58129643+gowthamlakshman94@users.noreply.github.com>
2025-12-02 15:59:58 +0530

    Update config.js

--- Canteen-Automation-System-Website/config.js
+++ Canteen-Automation-System-Website/config.js
@@ -1,4 +1,6 @@
 
 // config.js.template
-const BASE_URL = "http://cas-be.canteen-automation.svc.cluster.local:3000";
-const BASE_URL_FE = "http://cas-fe.canteen-automation.svc.cluster.local:3000";
+const BASE_URL = "http://localhost:3000";
+const BASE_URL_FE = "http://localhost:3000";
+const API_BASE = "http://localhost:3000";
+const BACKEND_BASE = "http://localhost:3000";
